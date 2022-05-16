e3de2a42b91a5717cecc826414c4caefe1044c80
6830960 <6830960@U702613.univali.br>
2022-05-16 15:00:56 -0300

    CM - Criado repositorio desafio01

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main() {
+        Email email = new Email();
+        View view = new View();
+        view.setVisible(true);
+
+    }
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,79 @@
+            
+public class Pessoa {
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(String dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+       public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private String nome;
+    private String login;
+    private String senha;
+    private String email;
+    private String telefone;
+    private String dataNasc;
+    private int status;
+    private int id;
+
+ 
+    
+
+}
